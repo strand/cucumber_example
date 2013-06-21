e0d18a5b421229ee97f5eb95f5f69433524b3c99
Strand McCutchen <strand@nird.us>
2013-06-21 10:47:59 -0700

    Created application and got the first feature passing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cucumber::Application.config.secret_token = 'a6229d6164bdbb7cfe0f197e80e9ebf47ae50bffcb5b98b8fec2b4e23eebd9e45b21ae0a05fbceb757cb331be3a88f2198357a1ee5f42611374d1ce56faa92e0'
